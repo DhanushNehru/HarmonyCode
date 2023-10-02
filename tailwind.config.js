82d751d30d6e9716510e1d9762cfb373ea86468d
Dhanush <dhanushxenocrate@gmail.com>
2023-10-02 20:30:25 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	mode: 'jit',
+	darkMode: 'class',
+	content: ['./pages/**/*.{js,jsx,ts,tsx}', './components/**/*.{js,jsx,ts,tsx}'],
+	theme: {
+		extend: {},
+	},
+	plugins: [],
+}
