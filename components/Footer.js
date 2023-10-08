bda2b8dff86d6dfbdcbae541904ca2c0d3b95d40
dipandhali2021 <dipandhali2021@gmail.com>
2023-10-08 22:45:44 +0530

    Added Hover Effect in Footer Section

--- components/Footer.js
+++ components/Footer.js
@@ -1,75 +1,103 @@
 import {
 	FacebookIcon,
-	FacebookShareButton, LinkedinIcon, LinkedinShareButton,
+	FacebookShareButton,
+	LinkedinIcon,
+	LinkedinShareButton,
 	PinterestIcon,
-	PinterestShareButton, RedditIcon,
+	PinterestShareButton,
+	RedditIcon,
 	RedditShareButton,
-	TwitterShareButton, TwitterIcon,
+	TwitterShareButton,
+	TwitterIcon,
 	WhatsappIcon,
-	WhatsappShareButton
-} from "next-share";
-import { AiFillGithub, AiOutlineInstagram, AiOutlineTwitter, AiOutlineLinkedin, AiOutlineYoutube } from "react-icons/ai"
-
-const Footer = () => {
+	WhatsappShareButton,
+  } from "next-share";
+  import {
+	AiFillGithub,
+	AiOutlineInstagram,
+	AiOutlineTwitter,
+	AiOutlineLinkedin,
+	AiOutlineYoutube,
+  } from "react-icons/ai";
+  
+  const Footer = () => {
 	return (
-		<footer className=" bg-white mt-24 p-4 flex  flex-col  md:flex-row  space-y-10 md:space-y-0  justify-between items-center  dark:bg-[#1a1a1a]">
-
-			<div className="text-md dark:text-gray-300 md:ml-2">
-				⚙️ Made with ❤️ by{" "}
-				<a
-					href="https://www.github.com/DhanushNehru"
-					className="underline"
-				>
-					Dhanush Nehru
-				</a>
+	  <footer className=" bg-white mt-24 p-4 flex  flex-col  md:flex-row  space-y-10 md:space-y-0  justify-between items-center  dark:bg-[#1a1a1a]">
+		<div className="text-md dark:text-gray-300 md:ml-2">
+		  ⚙️ Made with ❤️ by{" "}
+		  <a href="https://www.github.com/DhanushNehru" className="underline">
+			Dhanush Nehru
+		  </a>
+		</div>
+  
+		<div className="flex gap-4  flex-wrap">
+		  <a
+			href="https://github.com/DhanushNehru"
+			className="hover:shadow-2xl  hover:translate-y-1"
+		  >
+			<AiFillGithub size={40} />
+		  </a>
+		  <a
+			href="https://www.instagram.com/dhanush_nehru/"
+			className="hover:shadow-2xl  hover:translate-y-1"
+		  >
+			<AiOutlineInstagram size={40} />
+		  </a>
+		  <a
+			href="https://www.linkedin.com/in/dhanushnehru/"
+			className="hover:shadow-2xl  hover:translate-y-1"
+		  >
+			<AiOutlineLinkedin size={40} />
+		  </a>
+		  <a
+			href="https://twitter.com/Dhanush_Nehru"
+			className="hover:shadow-2xl  hover:translate-y-1"
+		  >
+			<AiOutlineTwitter size={40} />
+		  </a>
+		  <a
+			href="https://www.youtube.com/@dhanushnehru?sub_confirmation=1"
+			className="hover:shadow-2xl  hover:translate-y-1"
+		  >
+			<AiOutlineYoutube size={40} />
+		  </a>
+		</div>
+  
+		<div className="flex-col justify-center items-center space-y-2 mr-2">
+		  <h1>Share</h1>
+		  <div className="flex space-x-2">
+			<div className="hover:shadow-2xl  hover:translate-y-1">
+			  <PinterestShareButton url={"harmonycode.vercel.app/"}>
+				<PinterestIcon size={32} round />
+			  </PinterestShareButton>
 			</div>
-
-
-			<div className="flex gap-4  flex-wrap">
-				<a href="https://github.com/DhanushNehru"><AiFillGithub size={40} /></a>
-				<a href="https://www.instagram.com/dhanush_nehru/"><AiOutlineInstagram size={40} /></a>
-				<a href="https://www.linkedin.com/in/dhanushnehru/"><AiOutlineLinkedin size={40} /></a>
-				<a href="https://twitter.com/Dhanush_Nehru"><AiOutlineTwitter size={40} /></a>
-				<a href="https://www.youtube.com/@dhanushnehru?sub_confirmation=1"><AiOutlineYoutube size={40} /></a>
+			<div className="hover:shadow-2xl  hover:translate-y-1">
+			  <RedditShareButton url={"harmonycode.vercel.app/"}>
+				<RedditIcon size={32} round />
+			  </RedditShareButton>
 			</div>
-
-
-			<div className="flex-col justify-center items-center space-y-2 mr-2">
-				<h1>Share</h1>
-				<div className="flex-col space-x-2">
-					<FacebookShareButton
-						url={'harmonycode.vercel.app/'} >
-						<FacebookIcon size={32} round />
-					</FacebookShareButton>
-					<PinterestShareButton
-						url={'harmonycode.vercel.app/'} >
-						<PinterestIcon size={32} round />
-					</PinterestShareButton>
-					<RedditShareButton
-						url={'harmonycode.vercel.app/'} >
-						<RedditIcon size={32} round />
-					</RedditShareButton>
-					<WhatsappShareButton
-						url={'harmonycode.vercel.app/'} >
-						<WhatsappIcon size={32} round />
-					</WhatsappShareButton>
-					<LinkedinShareButton
-						url={'harmonycode.vercel.app/'} >
-						<LinkedinIcon size={32} round />
-					</LinkedinShareButton>
-					<TwitterShareButton
-						url={'harmonycode.vercel.app/'} 
-						style={{ marginRight: '40px' }}>
-						<TwitterIcon size={32} round />
-					</TwitterShareButton>
-				</div>
+			<div className="hover:shadow-2xl  hover:translate-y-1">
+			  <WhatsappShareButton url={"harmonycode.vercel.app/"}>
+				<WhatsappIcon size={32} round />
+			  </WhatsappShareButton>
 			</div>
-		</footer>
+			<div className="hover:shadow-2xl  hover:translate-y-1">
+			  <LinkedinShareButton url={"harmonycode.vercel.app/"}>
+				<LinkedinIcon size={32} round />
+			  </LinkedinShareButton>
+			</div>
+			<div className="hover:shadow-2xl  hover:translate-y-1">
+			  <TwitterShareButton
+				url={"harmonycode.vercel.app/"}
+				style={{ marginRight: "40px" }}
+			  >
+				<TwitterIcon size={32} round />
+			  </TwitterShareButton>
+			</div>
+		  </div>
+		</div>
+	  </footer>
 	);
-};
-
-export default Footer;
-
-
-
-
+  };
+  
+  export default Footer;
