985b72c75c53b623c73cfd2d7421ee61d1f2fd6b
j-mahapatra <jagannathm.official@gmail.com>
2023-10-09 10:48:56 +0530

    Fix #31: Make use of google signin
    - Added a check for signed in user before playing music.
    - Added a measurementId entry to firebaseConfig according to the latest
      firebase docs.

--- components/firebase.js
+++ components/firebase.js
@@ -9,6 +9,7 @@ const firebaseConfig = {
   storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
   messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID,
   appId: process.env.NEXT_PUBLIC_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_APP_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
